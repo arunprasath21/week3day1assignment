dadd05d4510dbc9ac655b2e8c4e62560fcb3a97c
arunprasath21 <107315878+arunprasath21@users.noreply.github.com>
2022-06-29 11:38:26 +0530

    Add files via upload

--- Students.java
+++ Students.java
@@ -3,27 +3,23 @@ package week3.Day1.org.Bank;
 public class Students {
 	 
 	public void getStudentInfo (int id) {
-			id = 12548;
+			 System.out.println("The ID : " + id );
 	}
 	public void getStudentInfo (int id ,String name) {
-		id = 1234;
-		name = "Arun";
+			System.out.println("The ID :"+ id  + " , "+"The Name : " + name);
 	}
 	public void getStudentInfo (String email ,int phonenumber ) {
-	 	email = "arunselvaraj21@gmail.com";
-	 	phonenumber = 984025864;
+	 		System.out.println("The email :" + email + " , " +"The PhoneNumber :" + phonenumber);
 	}
 	 	public static void main(String[] args) {
 			Students stud = new Students();
 			
-			stud.getStudentInfo(12548);
-			System.out.println(12548);
+			stud.getStudentInfo(256654);
 			
-			stud.getStudentInfo(1234, "Arun");
-			System.out.println(1234 + " Arun");
+			stud.getStudentInfo(1234, "ArunPrasath");
 			
 			stud.getStudentInfo("arunselvaraj21@gmail.com" , 984025864);
-			System.out.println("arunselvaraj21@gmail.com"  +  984025864);
+			
 	}
 		 
 }
